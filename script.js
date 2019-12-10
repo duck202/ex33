295c2e4192d0b7680b5622c54ef648b2e3d29637
duck202 <guyeldan@yahoo.com>
2019-12-10 21:27:56 +0200

    A

---
+++ script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function() {
+//     $("#link_a").click(function() {
+//         $("#main").load("maina.txt");
+//     });  
+
+//     $("#link_b").click(function() {
+//     $("#main").load("about.txt");
+//     });  
+
+//     $("#link_c").click(function() {
+//     $("#main").load("contact.txt");
+//     });  
+    
+// }); 
+
+
+$(document).ready(function(){
+      $("#main").load("maina.txt");
+    });
